56ed0d9e41741298a7dbfef4a79ad773e781ab16
Riobiss <riobisofc@gmail.com>
2025-09-15 12:06:28 -0300

    Validar campos name, email, password_hash e admin antes de criar usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import { v4 } from "uuid"
 import User from "../models/User.js"
+import Yup from "yup"
 /* 
 store => cadastrar/adicionar
 index => listar
@@ -10,6 +11,22 @@ delete => deletar
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(8),
+      admin: Yup.boolean(),
+    })
+    /*     if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validation fails" })
+    } */
+
+    try {
+      await schema.validateSync(req.body, { abortEarly: false })
+    } catch (err) {
+      return res.status(400).json({ error: err.errors })
+    }
+
     const { name, email, password_hash, admin } = req.body
 
     const user = await User.create({
