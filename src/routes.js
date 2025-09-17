be7348d780f436f5a13a9b9d43af10d012bdf0de
Riobiss <riobisofc@gmail.com>
2025-09-17 07:30:34 -0300

    adicionar multer para uploads de imagens

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express"
 import UserController from "./app/controllers/UserController.js"
 import SessionController from "./app/controllers/SessionController.js"
+import ProductController from "./app/controllers/ProductController.js"
+import multer from "multer"
+import multerConfig from "./config/multer.js"
 
+const upload = multer(multerConfig)
 const routes = new Router()
 
 routes.post("/users", UserController.store)
 
 routes.post("/sessions", SessionController.store)
+
+routes.post("/products", upload.single("file"), ProductController.store)
+
 export default routes
